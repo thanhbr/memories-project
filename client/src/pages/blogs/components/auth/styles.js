580133021ca5293e8ec2412da9f748a6eae1853a
ThanhMaThe <thanh.mathe@upos.vn>
2023-06-21 14:59:44 +0700

    update css

--- client/src/pages/blogs/components/auth/styles.js
+++ client/src/pages/blogs/components/auth/styles.js
@@ -1,5 +1,20 @@
 import { makeStyles } from '@mui/styles';
 
 export default makeStyles((theme) => ({
-  
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '24px'
+  },
+  root: {
+    '& .MuiTextField-root': {
+    },
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+  },
+  submit: {
+    marginTop: '16px !important'
+  }
 }))
--- client/src/pages/blogs/components/navbar/styles.js
+++ client/src/pages/blogs/components/navbar/styles.js
@@ -3,10 +3,40 @@ import { makeStyles } from '@mui/styles';
 export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
-    margin: "30px 0",
-    display: "flex",
-    flexDirection: "row !important",
+    margin: '30px 0',
+    display: 'flex !important',
+    flexDirection: 'row !important',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: '10px 50px',
+  },
+  heading: {
+    color: 'rgba(0,183,255, 1)',
+    textDecoration: 'none',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '400px',
+  },
+  profile: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '400px',
+  },
+  userName: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  brandContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  purple: {
+    // color: theme.palette.getContrastText(deepPurple[500]),
+    // backgroundColor: deepPurple[500],
   },
 }))
